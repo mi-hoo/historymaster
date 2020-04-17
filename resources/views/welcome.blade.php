2a9c3564d82aa03b8505aaece87f1a5710f90c01
Miho Asai <mihoo519@gmail.com>
2020-04-17 13:34:16 +0000

    second commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,99 +1,46 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<html lang="{{ app()->getLocale() }}">
     <head>
         <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-
-        <title>Laravel</title>
-
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
-
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
-                </div>
-            @endif
-
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
-            </div>
-        </div>
-    </body>
+@extends('layouts.admin')
+@section('content')
+    <div class="jumbotron">
+     <div class="clearfix">
+      <div class="float-left">
+       <h1>無料で学べる</h1>
+        <h1>中学歴史Web学習サイト</h1>
+        <p>サブテキストサブテキストサブテキストサブテキストサブテキストサブテキスト</br>
+        サブテキストサブテキストサブテキストサブテキストサブテキストサブテキスト</p>
+        <a class="btn btn-success btn-lg" href="#" role="button">学習を始める（ユーザー登録） »</a>
+       </div>
+      </div>
+     </div>
+    <div class="container">
+     <h2>HistoryMasterの特徴</h2>
+     <div class="feature">
+     <div class="row text-center">
+      <div class="col-md-4">
+        <i class="far fa-id-card fa-3x"></i>
+        <h5>必要なのはユーザー登録だけ！</br>すぐに学習を始めることができます</h5>
+      </div>
+      <div class="col-md-4">
+        <i class="far fa-check-square fa-3x"></i>
+        <h5>問題を解いたらワンクリックで採点</br>結果と解説を確認できます</h5>
+      </div>
+      <div class="col-md-4">
+        <i class="fas fa-robot fa-3x"></i>
+        <h5>間違えた問題は自動で記録</br>解き直しが簡単にできます</h5>
+      </div>
+      </div>
+      <div classs="row">
+    　 <div class="col-md-6 offset-md-3">
+        <a class="btn btn-secondary" href="#" role="button">使い方を詳しく見る</a>
+       </div>
+      </div>
+     </div>
+    </div>
+    
+@endsection
 </html>
