5e8ebfd78937bdb561ca88294614f390c25956f4
Miho Asai <mihoo519@gmail.com>
2020-04-19 13:53:03 +0000

    fix 4.19

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -9,25 +9,64 @@
 @section('content')
 <body class="body">
 <div class="container">
-<div class="card-deck">
-  <div class="card1">
-    <div class="card-header">古代</div>
-    <div class="card-body">
-      <p class="card-text">人類の誕生と古代文明</p>
-      <p class="card-text">人類の誕生と古代文明</p>
-      <p class="card-text">人類の誕生と古代文明</p>
-      <p class="card-text">人類の誕生と古代文明</p>
+ <div class="row">
+ <div class="col-md-4">
+    <div class="box">
+    <h3>古代</h3>
+     <p><a href="#">人類の誕生と古代文明</a></p>
+     <p><a href="#">日本の成り立ちと倭の文明</a></p>
+     <p><a href="#">大帝国の出現と律令国家の形成</a></p>
+     <p><a href="#">貴族社会の発展</a></p>
     </div>
-  </div>
-  <div class="card2">
-    <div class="card-header">近世</div>
-    <div class="card-body">
-      <p class="card-text">人類の誕生と古代文明</p>
-      <p class="card-text">人類の誕生と古代文明</p>
-      <p class="card-text">人類の誕生と古代文明</p>
-      <p class="card-text">人類の誕生と古代文明</p>
+ </div>
+ <div class="col-md-4">
+    <div class="box">
+    <h3>中世</h3>
+     <p><a href="#">問題タイトル</a></p>
+     <p><a href="#">問題タイトル</a></p>
+     <p><a href="#">問題タイトル</a></p>
+     <p><a href="#">問題タイトル</a></p>
+     <p><a href="#">問題タイトル</a></p>
     </div>
-  </div>
+ </div>
+ <div class="col-md-4">
+    <div class="box">
+    <h3>近世</h3>
+     <p><a href="#">問題タイトル</a></p>
+     <p><a href="#">問題タイトル</a></p>
+     <p><a href="#">問題タイトル</a></p>
+     <p><a href="#">問題タイトル</a></p>
+    </div>
+ </div>
+ </div>
+ <div class="row">
+ <div class="col-md-4">
+    <div class="box">
+    <h3>近代</h3>
+     <p><a href="#">問題タイトル</a></p>
+     <p><a href="#">問題タイトル</a></p>
+     <p><a href="#">問題タイトル</a></p>
+     <p><a href="#">問題タイトル</a></p>
+    </div>
+ </div>
+ <div class="col-md-4">
+    <div class="box">
+    <h3>現代</h3>
+     <p><a href="#">問題タイトル</a></p>
+     <p><a href="#">問題タイトル</a></p>
+     <p><a href="#">問題タイトル</a></p>
+     <p><a href="#">問題タイトル</a></p>
+    </div>
+ </div>
+ <div class="col-md-4">
+    <div class="box">
+    <h3>シャッフル問題</h3>
+     <p><a href="#">問題タイトル</a></p>
+     <p><a href="#">問題タイトル</a></p>
+     <p><a href="#">問題タイトル</a></p>
+     <p><a href="#">問題タイトル</a></p>
+    </div>
+ </div>
 </div>
 </div>
 </body>
