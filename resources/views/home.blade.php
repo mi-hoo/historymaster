a17036ea0a0db6344966d213f2f5cad5ca81aec0
Miho Asai <mihoo519@gmail.com>
2020-04-20 05:10:51 +0000

    fix 4.20

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -9,10 +9,12 @@
 @section('content')
 <body class="body">
 <div class="container">
+ <p><i class="far fa-grin"></i> こんにちは、{{ Auth::user()->name }}さん</p>
+ <h3>問題一覧</h3>
  <div class="row">
  <div class="col-md-4">
     <div class="box">
-    <h3>古代</h3>
+    <h4><i class="fas fa-crow"></i> 古 代</h4>
      <p><a href="#">人類の誕生と古代文明</a></p>
      <p><a href="#">日本の成り立ちと倭の文明</a></p>
      <p><a href="#">大帝国の出現と律令国家の形成</a></p>
@@ -21,7 +23,7 @@
  </div>
  <div class="col-md-4">
     <div class="box">
-    <h3>中世</h3>
+    <h4><i class="fas fa-horse-head"></i> 中 世</h4>
      <p><a href="#">問題タイトル</a></p>
      <p><a href="#">問題タイトル</a></p>
      <p><a href="#">問題タイトル</a></p>
@@ -31,7 +33,7 @@
  </div>
  <div class="col-md-4">
     <div class="box">
-    <h3>近世</h3>
+    <h4><i class="fas fa-vihara"></i> 近 世</h4>
      <p><a href="#">問題タイトル</a></p>
      <p><a href="#">問題タイトル</a></p>
      <p><a href="#">問題タイトル</a></p>
@@ -42,7 +44,7 @@
  <div class="row">
  <div class="col-md-4">
     <div class="box">
-    <h3>近代</h3>
+    <h4><i class="fas fa-industry"></i> 近 代</h4>
      <p><a href="#">問題タイトル</a></p>
      <p><a href="#">問題タイトル</a></p>
      <p><a href="#">問題タイトル</a></p>
@@ -51,7 +53,7 @@
  </div>
  <div class="col-md-4">
     <div class="box">
-    <h3>現代</h3>
+    <h4><i class="fas fa-space-shuttle"></i> 現 代</h4>
      <p><a href="#">問題タイトル</a></p>
      <p><a href="#">問題タイトル</a></p>
      <p><a href="#">問題タイトル</a></p>
@@ -60,7 +62,7 @@
  </div>
  <div class="col-md-4">
     <div class="box">
-    <h3>シャッフル問題</h3>
+    <h4><i class="fas fa-random"></i> シャッフル問題</h4>
      <p><a href="#">問題タイトル</a></p>
      <p><a href="#">問題タイトル</a></p>
      <p><a href="#">問題タイトル</a></p>
---
+++ resources/views/question.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="{{ app()->getLocale() }}">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+@extends('layouts.admin')
+@section('content')
+<body class="body">
+<div class="container">
+ 
+ <div class="row">
+ 
+ </div>
+
+</div>
+</body>
+@endsection
+</html>
