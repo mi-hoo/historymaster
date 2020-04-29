d8495631adffa500d79409bd7780fe6877c94b4f
Miho Asai <mihoo519@gmail.com>
2020-04-29 13:43:06 +0000

    fix 4.29

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -10,31 +10,26 @@
 <body class="body">
 <div class="container">
  <p><i class="far fa-grin"></i> こんにちは、{{ Auth::user()->name }}さん</p>
- <h3>問題一覧</h3>
- <div class="box2">
- <div class="col-md-4">
-    <div class="box1">
+ <div class="outer_box col-md-12">
+    <h3>問題一覧</h3>
+    <div class="row inner_box col-md-12">
     @foreach($categories as $category)
-    <div class="box">
-    <h4><i class="fas fa-crow"></i> {{ $category->name }}</h4>
-    @foreach($category->units as $unit)
-    <p><a href="{{ action('Admin\QuestionController@open', ['id' => $unit->id]) }}">{{ $unit->title }}</a></p>
+        <div class="box col-md-4">
+        <h4><i class="fas fa-crow"></i> {{ $category->name }}</h4>
+        @foreach($category->units as $unit)
+        <p><a href="{{ action('Admin\QuestionController@open', ['id' => $unit->id]) }}">{{ $unit->title }}</a></p>
+        @endforeach
+        </div>
     @endforeach
-    </div>
-    @endforeach
-    </div>
- </div>
- </div>
- <div class="col-md-4">
-    <div class="box">
-    <h4><i class="fas fa-random"></i> シャッフル問題</h4>
-     <p><a href="#">問題タイトル</a></p>
-     <p><a href="#">問題タイトル</a></p>
-     <p><a href="#">問題タイトル</a></p>
-     <p><a href="#">問題タイトル</a></p>
+        <div class="box col-md-4">
+        <h4><i class="fas fa-random"></i> シャッフル問題</h4>
+        <p><a href="#">問題タイトル</a></p>
+        <p><a href="#">問題タイトル</a></p>
+        <p><a href="#">問題タイトル</a></p>
+        <p><a href="#">問題タイトル</a></p>
+        </div>
     </div>
  </div>
-</div>
 </div>
 </body>
 @endsection
